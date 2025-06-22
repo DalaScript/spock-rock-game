2b383bbe57bb5f2fee140c58b80c920e63294e7f
DalaScript <nik.dalalishvili@gmail.com>
2025-06-22 15:45:47 +0400

    add functionality to reset game

--- script.js
+++ script.js
@@ -38,6 +38,20 @@ function resetSelected() {
   });
 }
 
+// Reset Score & playerChoice/computerChoice
+function resetAll() {
+  playerScoreNumber = 0;
+  computerScoreNumber = 0;
+  computerChoice = "";
+  playerScoreEl.textContent = playerScoreNumber;
+  computerScoreEl.textContent = computerScoreNumber;
+  playerChoiceEl.textContent = "";
+  computerChoiceEl.textContent = "";
+  resultText.textContent = "";
+
+  resetSelected();
+}
+
 // Random computer choice
 function computerRandomChoise() {
   const computerChoiceNumber = Math.random();
@@ -84,12 +98,10 @@ function displayComputerChoice() {
 
 // Check result, increase scores, update resultText
 function updateScore(playerChoice) {
-  console.log(playerChoice, computerChoice);
   if(playerChoice === computerChoice) {
     resultText.textContent = "It's a tie.";
   } else {
     const choice = choices[playerChoice];
-    console.log(choice.defeats.indexOf(computerChoice));
     if(choice.defeats.indexOf(computerChoice) > -1) {
       resultText.textContent = "You Won!";
       playerScoreNumber++;
@@ -139,3 +151,6 @@ function select(playerChoice) {
       break;
   }
 }
+
+// On startup, set initial values
+resetAll();
